be81e82c70d6ff46ae562e7eeccacdfc9afff288
hslim8888 <hslim8888@gmail.com>
2023-09-03 23:43:24 +0900

    Remove: unused url

--- mango_signup/urls.py
+++ mango_signup/urls.py
@@ -8,9 +8,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from user.views import UserViewSet
 
-router = DefaultRouter()
-router.register('user', UserViewSet)
-
 schema_view = get_schema_view(
    openapi.Info(
       title="Signup API",
@@ -21,11 +18,12 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+router = DefaultRouter()
+router.register('user', UserViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
