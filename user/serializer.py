597193e5883feeb76ab0e43223ae7a73a7e244b4
hslim8888 <hslim8888@gmail.com>
2023-09-03 23:13:58 +0900

    Refactor: UserSerializer 에 기본 로직(패스워드 해싱) 분리

--- user/serializer.py
+++ user/serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import User
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ['id', 'email', 'first_name', 'last_name', 'date_joined']
+        model = User
+        exclude = ["groups", "user_permissions"]
+        read_only_fields = ["is_superuser", "is_staff", "is_active"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
