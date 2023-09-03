6ce45ba7d91f40ce81c99827f6e217bcd1da714e
hslim8888 <hslim8888@gmail.com>
2023-09-03 17:59:39 +0900

    Feat: set swagger

--- mango_signup/urls.py
+++ mango_signup/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
@@ -8,10 +10,20 @@ from user.views import UserViewSet
 router = DefaultRouter()
 router.register('user', UserViewSet)
 
-
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Signup API",
+      default_version='v1',
+      description="API description",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
