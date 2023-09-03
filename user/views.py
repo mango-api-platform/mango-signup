b0269d501d39ede24f0b6492fa79da1630f3f79b
hslim8888 <hslim8888@gmail.com>
2023-09-03 23:14:25 +0900

    Refactor: viewset 에 jwt 관련 로직 분리

--- user/views.py
+++ user/views.py
@@ -1,10 +1,23 @@
-from rest_framework import viewsets
-from rest_framework import permissions
-from .models import CustomUser
-from .serializer import CustomUserSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import User
+from .serializer import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all().order_by('-date_joined')
-    serializer_class = CustomUserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # jwt 토큰
+        access_token = 'test' # str(RefreshToken.for_user(user).access_token)
+
+        return Response({'access_token': access_token, **serializer.data}, status=status.HTTP_201_CREATED,
+                        headers=(self.get_success_headers(serializer.data)))
